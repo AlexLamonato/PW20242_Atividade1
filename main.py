e7ce7e50d9a9c33c4df3013cee09e6327af95411
AlexLamonato <alexlamonatofilho@gmail.com>
2024-10-01 21:57:32 -0300

    tetando terminar o main.py

--- main.py
+++ main.py
@@ -7,8 +7,22 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def get_root(request: Request):
+async def get_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/cadastrao")
+async def cadastro(request: Request):
+    return templates.TemplateResponse("Cadastrar.html", {"request": request})
+
+@app.post("/post_cadastro")
+async def postar_cadastro(
+    nome: str = Form(...), 
+    descricao: str = Form(...),
+    estoque: str = Form(...),
+    preco: str = Form(...), 
+    categoria: str = Form(...), 
+    confirmacao_senha: str = Form(...))
+    return RedirectResponse("/cadastro", 303)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
