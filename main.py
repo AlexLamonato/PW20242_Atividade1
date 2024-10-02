3d401b993ac7453aaf23edfa7e18a801441019a3
AlexLamonato <alexlamonatofilho@gmail.com>
2024-10-01 21:22:06 -0300

    Main.py has been fully tested and deployed, and is now in operation.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from re import template
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def get_root(request: Request):
-    return template.TemplatesResponse("index.html", {"request": request})
+def get_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
