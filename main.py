6361f3aa6e7029111d79c38ac339b3c694bad9ee
AlexLamonato <alexlamonatofilho@gmail.com>
2024-10-08 20:04:04 -0300

    Termino Atividade 6

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from re import template
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn
+from util import salvar_cadastro
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -10,19 +11,30 @@ templates = Jinja2Templates(directory="templates")
 async def get_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/cadastrao")
+@app.get("/cadastro")
 async def cadastro(request: Request):
-    return templates.TemplateResponse("Cadastrar.html", {"request": request})
+    return templates.TemplateResponse("Cadastro.html", {"request": request})
 
-@app.post("/post_cadastro")
-async def postar_cadastro(
+@app.post("/dados/cadastros")
+async def postar_cadastro(request: Request,
     nome: str = Form(...), 
     descricao: str = Form(...),
     estoque: str = Form(...),
     preco: str = Form(...), 
-    categoria: str = Form(...), 
-    confirmacao_senha: str = Form(...))
-    return RedirectResponse("/cadastro", 303)
+    categoria: str = Form(...)):
+    
+    try:
+        preco_float = float(preco)
+    except ValueError:
+        # Preço inválido, redireciona de volta para a página de cadastro
+        return RedirectResponse("/cadastro", 303)
+
+    salvar_cadastro(nome, descricao, estoque, preco_float, categoria)
+    return RedirectResponse("/cadastro_recebido", 303)
+
+@app.get("/cadastro_recebido")
+async def cadastro_recebido(request: Request):
+    return templates.TemplateResponse("cadastro_recebido.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
---
+++ util.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+NOME_PASTA_HTML="html/"
+NOME_PASTA_CONTATOS="dados/contatos/"
+NOME_PASTA_CADASTROS="dados/cadastros/"
+
+def ler_html(nome_arquivo: str) -> str:
+    caminho_arquivo_html = f"{NOME_PASTA_HTML}{nome_arquivo}.html"
+    with open(caminho_arquivo_html, "r", encoding="utf-8") as arquivo:
+        conteudo_html = arquivo.read()
+    return conteudo_html
+
+def salvar_cadastro(nome, descricao, estoque, preco, categoria):
+    agora = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    nome_arquivo = f"{NOME_PASTA_CADASTROS}cadastro_{agora}.txt"
+    conteudo = f"Nome: {nome}\ndescricao: {descricao} \n estoque: {estoque} \n preco: {preco} \n categoria: {categoria}"
+    with open(nome_arquivo, "w") as arquivo:
+        arquivo.write(conteudo)
